39bdfea53d5dda3984012902cc1d508aeffb8786
Arevico <Arevico@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-11 15:50:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-promoter-lightbox/tags/1.1@436391 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sp.php
+++ sp.php
@@ -3,7 +3,7 @@
    Plugin Name: Facebook Page Promoter Lightbox
    Plugin URI: http://arevico.com/kb-facebook-page-lightbox/
    Description:  All your visitors should know about your facebook page and tell their friends. With this plugin you can display a preconfigured Facebook Page-Like Box inside a lightbox.
-   Version: 1.0
+   Version: 1.1
    Author: Arevico
    Author URI: http://arevico.com/
    Copyright: 2011, Arevico
@@ -18,12 +18,12 @@ function lb_appender($content){
 			</script><script>FB.init();</script><a id="inline" href="#data" style="display: none;">Show</a><div style="display:none">
 			<div id="data" style=""><fb:fan profile_id="' . $options['fb_id'] .  '" connections="6" width="400" height="180" frameborder="none"></fb:fan></div></div>';
 	$lr .= js_localize("lb_l_ret",$options);
-	$lr .= genScript(array('fancybox/launch.js'));
+	$lr .= genScript(array('scs/launch.js'));
 
 	if (!$options['fancybox']) {
-		$content .= genScript(array('fancybox/jquery.fancybox-1.3.4.js'));
+		$content .= genScript(array('scs/scs.js'));
 	}
-		$content .= genStyle(array('fancybox/jquery.fancybox-1.3.4.css')) . $lr;
+		$content .= genStyle(array('scs/scs.css')) . $lr;
 
 	}
 	/* Remove shorttags*/
@@ -38,7 +38,7 @@ function lb_appender($content){
 function my_jq_apper() {
 	wp_deregister_script( 'jquery' );
 	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js');
-	wp_enqueue_script( 'jquery' );
+	wp_enqueue_	ript( 'jquery' );
 }
 
 add_filter('the_content', 'lb_appender');
