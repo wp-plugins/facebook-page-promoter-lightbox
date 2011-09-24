259d09f2cae33e85e2baa3fe5a1fcd10edf136e8
Arevico <Arevico@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-24 20:45:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-promoter-lightbox/tags/1.1@443010 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -43,10 +43,11 @@ function arv_fbx_options_do_page() {
     </div>
 	<?php } ?>
 
-	<div class="updated settings-error">
-		<p><strong>Do more with Social Media: &nbsp;&nbsp;&nbsp;<a href="http://arevico.com/out/?p=hyperfacebook2">Check Hyper Facebook Traffic. It will greatly help your social media presence And ultimately increase your traffic.</a><br /><a href="http://arevico.com/out/?p=hyperfacebook2" target="_blank"><img src="http://www.hyperfbtraffic.com/images/GraphicsBanners/banner-4.jpg" width="468" height="60" border=0></a></strong>
-		</p>		
+		<div class="updated settings-error">
+			<p><strong>Do more with Social Media: &nbsp;&nbsp;&nbsp;<a href="http://arevico.com/out/?p=hyperfacebook2">Check Hyper Facebook Traffic. It will greatly help your social media presence And ultimately increase your traffic.</a><br /><a href="http://arevico.com/out/?p=hyperfacebook2" target="_blank"><img src="http://www.arevico.com/banner.php" width="468" height="60" border=0></a></strong>
+		</p>			
 	</div>
+
 
 	<h2>Options</h2>
 		<form method="post" action="options.php" id="fl">
