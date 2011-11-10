501e94cf43ff513fb1e4f4b507799a849331191e
Arevico <Arevico@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-10 23:37:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-promoter-lightbox/trunk@461553 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- arevico_options.php
+++ arevico_options.php
@@ -60,6 +60,7 @@ function __construct(){
 		</p>
 		<strong>Do more with Social Media: &nbsp;&nbsp;&nbsp;<a href="http://b0e48wu1-gmc2tae-3j1k1k74y.hop.clickbank.net/">Check Hyper Facebook Traffic. It will greatly help your social media presence And ultimately increase your traffic.</a><br><a href="http://b0e48wu1-gmc2tae-3j1k1k74y.hop.clickbank.net/"><img src="http://arevico.com/ad/banner.php" style="border-style: none;"></a></strong>
 		</p>
+		<a href="http://arevico.us2.list-manage.com/subscribe?u=e23c318ca9665b7616c745ccb&id=13e68d454d">Banner Free Version</a>
 		`</div>
 
 
