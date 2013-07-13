fdc899a43f3cf6611a43739d63b5da17579a384b
Arevico <Arevico@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-13 21:51:04 +0000

    forgot to update version numbers in main plugin header to reflect new major version
    git-svn-id: https://plugins.svn.wordpress.org/facebook-page-promoter-lightbox/tags/2.6.2@740386 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
    Plugin Name: Facebook Page Promoter Lightbox
    Plugin URI: http://wordpress.org/extend/plugins/facebook-page-promoter-lightbox/
    Description:  All your visitors should know about your facebook page and tell their friends. With this plugin you can display a preconfigured Facebook Page-Like Box inside a lightbox.
-   Version: 2.6.1
+   Version: 2.6.2
    Author: Arevico
    Author URI: http://arevico.com/sp-facebook-lightbox-premium/?
-   Copyright: 2012, Arevico
+   Copyright: 2013, Arevico
 */
 
 $current_loc = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
